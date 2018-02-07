63295ff6a73b839d8013bf87ce3df60d5377acef
cmurphy <cmurphy@glunzbeers.com>
2018-02-07 13:06:05 -0600

    'last work for today'

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyCnDUaqgLL2f1S7-B1otVvUVHeolHqTsX0';
+
+const App = () => {
+  return (
+  <div>
+    <SearchBar />
+  </div>
+);}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+  <App />,
+  document.querySelector('.container')
+); 
