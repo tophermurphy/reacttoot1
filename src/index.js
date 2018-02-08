944abc5a7c498304276680750d1f13a924f6d848
tophermurphy <tophermurphy@gmail.com>
2018-02-07 20:56:45 -0600

    'yawn'

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCnDUaqgLL2f1S7-B1otVvUVHeolHqTsX0';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch(
+      {key: API_KEY, term: 'surfboards'}, 
+      (videos) => {
+        this.setState({ videos });
+      }
+    );
+
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
