8d0e09d798f0d20098e1e613a9bf6d4f21e4fbcd
tophermurphy <tophermurphy@gmail.com>
2018-02-10 09:25:02 -0600

    'random commit'

--- src/index.js
+++ src/index.js
@@ -12,23 +12,35 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
 
+    this.videoSearch('surfboards');
+
+  }
+  
+  videoSearch(term){
     YTSearch(
-      {key: API_KEY, term: 'surfboards'}, 
+      {key: API_KEY, term: term}, 
       (videos) => {
-        this.setState({ videos });
+        this.setState({ 
+          videos: videos,
+          selectedVideo: videos[0]
+        });
       }
     );
-
   }
 
   render(){
     return (
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={ term => this.videoSearch(term) } />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
